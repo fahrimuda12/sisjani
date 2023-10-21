7c699a575ca03e30d8cdc94114f091ab790ba91f
fahrimuda12 <profesorrob01@gmail.com>
2023-10-21 17:29:17 +0700

    Fix JS

--- resources/views/admin/components/header.blade.php
+++ resources/views/admin/components/header.blade.php
@@ -4,15 +4,16 @@
             <div class="mobile-toggle-menu"><i class='bx bx-menu'></i>
             </div>
 
-              {{-- <div class="position-relative search-bar d-lg-block d-none" data-bs-toggle="modal" data-bs-target="#SearchModal">
+            {{-- <div class="position-relative search-bar d-lg-block d-none" data-bs-toggle="modal" data-bs-target="#SearchModal">
                 <input class="form-control px-5" disabled type="search" placeholder="Search">
                 <span class="position-absolute top-50 search-show ms-3 translate-middle-y start-0 top-50 fs-5"><i class='bx bx-search'></i></span>
               </div> --}}
 
 
-              <div class="top-menu ms-auto">
+            <div class="top-menu ms-auto">
                 <ul class="navbar-nav align-items-center gap-1">
-                    <li class="nav-item mobile-search-icon d-flex d-lg-none" data-bs-toggle="modal" data-bs-target="#SearchModal">
+                    <li class="nav-item mobile-search-icon d-flex d-lg-none" data-bs-toggle="modal"
+                        data-bs-target="#SearchModal">
                         <a class="nav-link" href="avascript:;"><i class='bx bx-search'></i>
                         </a>
                     </li>
@@ -264,15 +265,16 @@
                                       </div>
                                     </a>
                                  </div>
-        
+
                               </div><!--end row-->
-        
+
                             </div>
                         </div>
                     </li> --}}
 
                     <li class="nav-item dropdown dropdown-large">
-                        <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="#" data-bs-toggle="dropdown"><span class="alert-count">7</span>
+                        <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="#"
+                            data-bs-toggle="dropdown"><span class="alert-count">7</span>
                             <i class='bx bx-bell'></i>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end">
@@ -286,11 +288,12 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="assets/images/avatars/avatar-1.png" class="msg-avatar" alt="user avatar">
+                                            <img src="assets/images/avatars/avatar-1.png" class="msg-avatar"
+                                                alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Daisy Anderson<span class="msg-time float-end">5 sec
-                                        ago</span></h6>
+                                                    ago</span></h6>
                                             <p class="msg-info">The standard chunk of lorem</p>
                                         </div>
                                     </div>
@@ -301,7 +304,7 @@
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">New Orders <span class="msg-time float-end">2 min
-                                        ago</span></h6>
+                                                    ago</span></h6>
                                             <p class="msg-info">You have recived new orders</p>
                                         </div>
                                     </div>
@@ -309,11 +312,12 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="assets/images/avatars/avatar-2.png" class="msg-avatar" alt="user avatar">
+                                            <img src="assets/images/avatars/avatar-2.png" class="msg-avatar"
+                                                alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Althea Cabardo <span class="msg-time float-end">14
-                                        sec ago</span></h6>
+                                                    sec ago</span></h6>
                                             <p class="msg-info">Many desktop publishing packages</p>
                                         </div>
                                     </div>
@@ -325,7 +329,7 @@
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Account Created<span class="msg-time float-end">28 min
-                                        ago</span></h6>
+                                                    ago</span></h6>
                                             <p class="msg-info">Successfully created new email</p>
                                         </div>
                                     </div>
@@ -335,8 +339,8 @@
                                         <div class="notify bg-light-info text-info">Ss
                                         </div>
                                         <div class="flex-grow-1">
-                                            <h6 class="msg-name">New Product Approved <span
-                                        class="msg-time float-end">2 hrs ago</span></h6>
+                                            <h6 class="msg-name">New Product Approved <span class="msg-time float-end">2
+                                                    hrs ago</span></h6>
                                             <p class="msg-info">Your new product has approved</p>
                                         </div>
                                     </div>
@@ -344,22 +348,25 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="assets/images/avatars/avatar-4.png" class="msg-avatar" alt="user avatar">
+                                            <img src="assets/images/avatars/avatar-4.png" class="msg-avatar"
+                                                alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Katherine Pechon <span class="msg-time float-end">15
-                                        min ago</span></h6>
+                                                    min ago</span></h6>
                                             <p class="msg-info">Making this the first true generator</p>
                                         </div>
                                     </div>
                                 </a>
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
-                                        <div class="notify bg-light-success text-success"><i class='bx bx-check-square'></i>
+                                        <div class="notify bg-light-success text-success"><i
+                                                class='bx bx-check-square'></i>
                                         </div>
                                         <div class="flex-grow-1">
-                                            <h6 class="msg-name">Your item is shipped <span class="msg-time float-end">5 hrs
-                                        ago</span></h6>
+                                            <h6 class="msg-name">Your item is shipped <span class="msg-time float-end">5
+                                                    hrs
+                                                    ago</span></h6>
                                             <p class="msg-info">Successfully shipped your item</p>
                                         </div>
                                     </div>
@@ -371,7 +378,7 @@
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">New 24 authors<span class="msg-time float-end">1 day
-                                        ago</span></h6>
+                                                    ago</span></h6>
                                             <p class="msg-info">24 new authors joined last week</p>
                                         </div>
                                     </div>
@@ -379,11 +386,12 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="assets/images/avatars/avatar-8.png" class="msg-avatar" alt="user avatar">
+                                            <img src="assets/images/avatars/avatar-8.png" class="msg-avatar"
+                                                alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Peter Costanzo <span class="msg-time float-end">6 hrs
-                                        ago</span></h6>
+                                                    ago</span></h6>
                                             <p class="msg-info">It was popularised in the 1960s</p>
                                         </div>
                                     </div>
@@ -585,7 +593,8 @@
                 </ul>
             </div>
             <div class="user-box dropdown px-3">
-                <a class="d-flex align-items-center nav-link dropdown-toggle gap-3 dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="d-flex align-items-center nav-link dropdown-toggle gap-3 dropdown-toggle-nocaret"
+                    href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="{{ asset('assets/images/avatars/avatar-2.png') }}" class="user-img" alt="user avatar">
                     <div class="user-info">
                         <p class="user-name mb-0">{{ Auth::user()->name }}</p>
@@ -593,9 +602,11 @@
                     </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
-                    <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-user fs-5"></i><span>Profile</span></a>
+                    <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
+                                class="bx bx-user fs-5"></i><span>Profile</span></a>
                     </li>
-                    <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-cog fs-5"></i><span>Settings</span></a>
+                    <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
+                                class="bx bx-cog fs-5"></i><span>Settings</span></a>
                     </li>
                     {{-- <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-home-circle fs-5"></i><span>Dashboard</span></a>
                     </li>
@@ -604,10 +615,11 @@
                     <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-download fs-5"></i><span>Downloads</span></a>
                     </li>
                     <li> --}}
-                        <div class="dropdown-divider mb-0"></div>
+                    <div class="dropdown-divider mb-0"></div>
                     </li>
                     <li>
-                        <a class="dropdown-item d-flex align-items-center" href="/logout"><i class="bx bx-log-out-circle"></i><span>Logout</span></a>
+                        <a class="dropdown-item d-flex align-items-center" href="/logout"><i
+                                class="bx bx-log-out-circle"></i><span>Logout</span></a>
                     </li>
                 </ul>
             </div>
